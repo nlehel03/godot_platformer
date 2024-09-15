0232418c979bf1cbb1692f53c72627ad1599766c
nlehel03 <nlehel03@gmail.com>
2024-09-15 23:22:57 +0200

    2024.09.15 4

--- elso/scenes/gameobjects/main_character.gd
+++ elso/scenes/gameobjects/main_character.gd
@@ -5,6 +5,7 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -900.0
 @onready var sprite_2d = $Sprite2D
 var gravity=ProjectSettings.get_setting("physics/2d/default_gravity")
+var jump_count=0
 
 func jump():
 	velocity.y=JUMP_VELOCITY
@@ -13,21 +14,37 @@ func jump_side(x):
 	velocity.y=JUMP_VELOCITY
 	velocity.x=x
 
+
+
+
 func _physics_process(delta: float) -> void:
-	#animaciok
-	if(velocity.x>1 || velocity.x<-1):
-		sprite_2d.animation="running"
-	else:
-		sprite_2d.animation="default"
+	
+	
+	
+		
+		
+		
+		
 	# Add the gravity.
-	if not is_on_floor():
+	if is_on_floor():
+		jump_count=0
+		#animaciok
+		if(velocity.x>1 || velocity.x<-1):
+			sprite_2d.animation="running"
+		else:
+			sprite_2d.animation="default"
+	else:
 		velocity += get_gravity() * delta
-		sprite_2d.animation="jumping"
+		if (jump_count==2):
+			sprite_2d.animation="double_jumping"
+		else:
+			sprite_2d.animation="jumping"
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and jump_count<2:
 		velocity.y = JUMP_VELOCITY
-
+		jump_count +=1
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
