910c007bf719f4da2ee3813bacd7095475d8ae3a
nlehel03 <nlehel03@gmail.com>
2024-09-28 19:12:40 +0200

    2024.09.27

--- elso/scenes/levels/game_manager.gd
+++ elso/scenes/levels/game_manager.gd
@@ -1,13 +1,25 @@
 extends Node
-@onready var points_label: Label = $"../UI/Panel/PointsLabel"
+@onready var gui_scene =preload("res://scenes/menu/GUI.tscn")
+var gui_instance: Node =null
+var points_label: Label = null
+
+func _ready() ->void:
+	gui_instance=gui_scene.instantiate()
+	get_tree().root.add_child(gui_instance)
+	gui_instance.name="GUI"
+	points_label = gui_instance.get_node("UI/Panel/PointsLabel")
+	print("gui példányosítva:",get_tree().root.get_child_count())
+
+
 @export var hearts: Array[Node]
+
 
 var points=0
 var lives=3
 
 func decrease_health():
 	lives-=1
-	for i in 3:
+	for i in range(3):
 		if(i<lives):
 			hearts[i].show()
 		else:
@@ -18,5 +30,8 @@ func decrease_health():
 func add_point():
 	points+=1
 	print(points)
-	points_label.text="Points: "+str(points)
+	if points_label:
+		points_label.text="Points: "+str(points)
+	else:
+		points_label.text="points_label nem található"
 	
